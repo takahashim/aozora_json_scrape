3a61627666d4fddb7c97fd6c1144d6ebce5d8018
takahashim <maki@rubycolor.org>
2020-04-14 20:54:21 +0900

    refactor

--- gen_card_list.rb
+++ gen_card_list.rb
@@ -11,15 +11,15 @@ require 'json'
 
 ATTRS = {title: "作品名", title_kana: "作品名読み", subtitle: "副題", subtitle_kana: "副題読み",
          title_en: "原題", person_name: "著者名",
-         collection:"作品集名", collection_kana:"作品集名読み",
-         cat: "分類", work_detail: "作品について", kana_type: "文字遣い種別", note: "備考",
-         first_appearance: "初出",kana_type2:"仮名遣い種別",
+         collection: "作品集名", collection_kana: "作品集名読み",
+         class: "分類", work_note: "作品について", kana_type: "文字遣い種別", note: "備考",
+         first_appearance: "初出",kana_type2: "仮名遣い種別",
          author_name: "作家名", author_kana: "作家名読み",
-         original_book: "底本", publisher: "出版社", first_published_on: "初版発行日",
+         original_book: "底本", publisher: "出版社", first_edition: "初版発行日",
          input_edition: "入力に使用", proof_edition: "校正に使用",
          base_book: "底本の親本",
          input: "入力", proofread: "校正",
-         author_en: "ローマ字表記", born_on: "生年", died_on: "没年", author_detail: "人物について",
+         author_en: "ローマ字表記", born_on: "生年", died_on: "没年", author_note: "人物について",
          site_name: "サイト名", url: "URL"}
 ATTRS_R = ATTRS.invert
 
