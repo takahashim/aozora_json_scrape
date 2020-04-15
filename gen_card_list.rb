5579dc19c610b4fd671348441168b24f740895d2
takahashim <maki@rubycolor.org>
2020-04-15 14:44:59 +0900

    refactor

--- gen_card_list.rb
+++ gen_card_list.rb
@@ -13,7 +13,7 @@ ATTRS = {title: "作品名", title_kana: "作品名読み", subtitle: "副題", 
          title_en: "原題", person_name: "著者名",
          collection: "作品集名", collection_kana: "作品集名読み",
          class: "分類", work_note: "作品について", kana_type: "文字遣い種別", note: "備考",
-         first_appearance: "初出",kana_type2: "仮名遣い種別",
+         first_appearance: "初出", kana_type2: "仮名遣い種別",
          author_name: "作家名", author_kana: "作家名読み",
          original_book: "底本", publisher: "出版社", first_edition: "初版発行日",
          input_edition: "入力に使用", proof_edition: "校正に使用",
@@ -39,25 +39,26 @@ def parse_table(doc, summary)
     data = {}
     t.children.each do |node|
       if node.class == Nokogiri::XML::Element
+        attr = nil
         node.children.each do |child|
           if child.name == "td"
             if child["class"] == "header"
               value = child.text.strip.sub(/：$/, "")
               if summary == :author && value == "分類"
-                @attr = :role
+                attr = :role
               else
-                @attr = ATTRS_R[value]
+                attr = ATTRS_R[value]
               end
             else
-              if [:author_detail, :note, :work_detail].member?(@attr)
-                data[@attr] = child.children.to_s.strip
+              if [:author_detail, :note, :work_detail].member?(attr)
+                data[attr] = child.children.to_s.strip
               else
-                data[@attr] = child.text.strip
+                data[attr] = child.text.strip
               end
 
-              if [:author_name, :person_name].member?(@attr)
+              if [:author_name, :person_name].member?(attr)
                 url = child.css("a").attribute("href").value
-                data[:author_num] = parse_num_from_person_path(url)
+                data[:author_id] = parse_num_from_person_path(url)
               end
             end
           end
@@ -97,14 +98,53 @@ def parse_download(doc)
   data
 end
 
-def parse_card(path)
+def read_content(path)
   content = File.read(path)
   unless content.valid_encoding?
     content = File.read(path, encoding: "euc-jp")
   end
+  content
+end
+
+##
+# remove kana_type2
+#
+# There are notation distortions of "仮名遣い種別" and "文字遣い種別".
+#
+def fix_kana_type(card)
+  work = card[:work]
+  if work[:kana_type2]
+    work[:kana_type] = work[:kana_type2]
+    work.delete(:kana_type2)
+  end
+end
+
+def merge_original_book(card)
+  card[:original_book] ||= []
+  card[:base_book] ||= []
+  card[:original_book].each do |elem|
+    if elem[:original_book]
+      elem[:book_title] = elem[:original_book]
+      elem.delete(:original_book)
+    end
+    elem[:booktype] = "底本"
+  end
+  card[:base_book].each do |elem|
+    if elem[:base_book]
+      elem[:book_title] = elem[:base_book]
+      elem.delete(:base_book)
+    end
+    elem[:booktype] = "底本の親本"
+  end
+  card[:original_book].concat(card[:base_book])
+  card.delete(:base_book)
+end
+
+def parse_card(path)
+  content = read_content(path)
   doc = Nokogiri::HTML(content)
 
-  {
+  card = {
     title: parse_table(doc, :title)[0],
     work: parse_table(doc, :work)[0],
     author: parse_table(doc, :author),
@@ -114,6 +154,16 @@ def parse_card(path)
     download: parse_download(doc),
     site: parse_table(doc, :site)
   }
+  fix_kana_type(card)
+  merge_original_book(card)
+  card
+end
+
+def parse_person_and_work(path)
+  path =~ %r|cards/(\d+)/card(\d+)\.html|
+  person_num = $1.to_i
+  work_num = $2.to_i
+  return person_num, work_num
 end
 
 def parse_all_cards
@@ -122,9 +172,7 @@ def parse_all_cards
   i = 0
   Dir.glob("cards/*/card*.html").sort.each do |d|
     begin
-      d =~ %r|cards/(\d+)/card(\d+)\.html|
-      person_num = $1.to_i
-      work_num = $2.to_i
+      person_num, work_num = parse_person_and_work(d)
 
       # num <= 4 まではダミーなので無視
       if person_num > 4
