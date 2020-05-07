0b8602ae7f949bc3fd0d3b78aa0020788f1b9196
takahashim <maki@rubycolor.org>
2020-05-08 00:15:41 +0900

    add make_sakuhin_list()

--- gen_whatsnew.rb
+++ gen_whatsnew.rb
@@ -46,6 +46,31 @@ def save_whatsnew_json(works, year)
   File.write("whatsnew#{year}.json", JSON.dump(works_year)+"\n")
 end
 
+def make_sakuhin_list(csv_file)
+  sakuhin_list = {}
+  CSV.foreach(csv_file, headers: true) do |row|
+    work_id, title, subtitle, kana_type, published_on, card_url, f_name, l_name, role, input, proofread =
+                                                                                   row[0], row[1], row[4], row[9], row[11], row[13], row[15], row[16], row[23], row[43], row[44]
+    if works[work_id]
+      work = works[work_id]
+      if work[:title] != title || work[:subtitle] != subtitle || work[:published_on] != published_on || work[:card_url] != card_url
+        raise "Invalid data: #{row.inspect}"
+      end
+    else
+      works[work_id] = {work_id: work_id, title: title, subtitle: subtitle,
+                        published_on: published_on, card_url: card_url,
+                        input: input, proofread: proofread}
+    end
+    works[work_id][:author] ||= []
+    works[work_id][:author] << {author_name: "#{f_name} #{l_name}", role: role}
+  end
+
+  ## 公開日順と作品ID順で一意にソート
+  works_sorted = works.values.sort_by{ |work| [work[:published_on], work[:work_id]] }.reverse
+
+  works_sorted
+end
+
 
 # 作品ID,作品名,作品名読み,ソート用読み,副題,副題読み,原題,初出,分類番号,文字遣い種別,作品著作権フラグ,公開日,最終更新日,図書カードURL,人物ID,姓,名,姓読み,名読み,姓読みソート用,名読みソート用,姓ローマ字,名ローマ字,役割フラグ,生年月日,没年月日,人物著作権フラグ,底本名1,底本出版社名1,底本初版発行年1,入力に使用した版1,校正に使用した版1,底本の親本名1,底本の親本出版社名1,底本の親本初版発行年1,底本名2,底本出版社名2,底本初版発行年2,入力に使用した版2,校正に使用した版2,底本の親本名2,底本の親本出版社名2,底本の親本初版発行年2,入力者,校正者,テキストファイルURL,テキストファイル最終更新日,テキストファイル符号化方式,テキストファイル文字集合,テキストファイル修正回数,XHTML/HTMLファイルURL,XHTML/HTMLファイル最終更新日,XHTML/HTMLファイル符号化方式,XHTML/HTMLファイル文字集合,XHTML/HTMLファイル修正回数
 
@@ -60,3 +85,6 @@ prev_year = (curr_year.to_i - 1).to_s
 
 save_whatsnew_json(works, curr_year)
 save_whatsnew_json(works, prev_year)
+
+## sakuhin
+sakuhin_sorted = make_sakuhin_list(csv_file)
