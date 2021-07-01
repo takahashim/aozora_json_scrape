c48df9244c2ce9044a1b674ff9a8d1e2f8902ec3
aewshyae <1528058+aewshyae@users.noreply.github.com>
2021-07-02 08:07:38 +0900

    update gen_index_work_ids_by_bibclass.rb gen_index_work_ids_by_initial.rb to output card detail

--- gen_index_work_ids_by_bibclass.rb
+++ gen_index_work_ids_by_bibclass.rb
@@ -1,11 +1,12 @@
 #! /usr/bin/env ruby
 
 # gen_card_list.rb で生成されたJSON card.json から、
-# 十進法分類別の作品IDリストを生成する
+# 十進法分類別の作品リストを生成する
 #
 # Usage: ruby [this file] > work_ids_by_bibclass.json
 
 require "json"
+require_relative "util.rb"
 
 INPUT_JSON_FILENAME='./card.json'
 
@@ -19,11 +20,20 @@ def chunk_cards_by_bibclass
       work_id = card.dig("title","work_id")
       next if bibclass.nil? || work_id.nil?
 
+      work = {
+        id: work_id,
+        title: card.dig("title", "title"),
+        subtitle: card.dig("title", "subtitle"),
+        kana_type: card.dig("work", "kana_type"),
+        author: card.dig("title", "person_name"),
+        title_kana: canonicalize_to_kana(card.dig("title", "title_kana")),
+      }
+
       canonicalize_bibclass(bibclass).each do |ndc|
         if output.has_key?(ndc)
-          output[ndc].push(work_id)
+          output[ndc].push(work)
         else
-          output[ndc] = [work_id]
+          output[ndc] = [work]
         end
       end
     end
@@ -46,7 +56,11 @@ end
 result = chunk_cards_by_bibclass
 
 result.each do |k,v|
-  v.sort!
+  v.sort! do |a,b|
+    a[:id] <=> b[:id]
+  end.sort! do |a,b|
+    a[:title_kana] <=> b[:title_kana]
+  end
 end
 result = result.sort_by{ |k, v| [k, v.size]}
 
